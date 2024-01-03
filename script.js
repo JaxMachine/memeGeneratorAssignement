34c3853a72f21486b596c8e87d12c6e8e7e7109f
JaxMachine <newtradesofjack@gmail.com>
2024-01-02 23:34:10 -0600

    Finalized Generator

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
 const formElement = document.querySelector("form");
 const submitButton = document.getElementById("submitButton");
 const imageDisplay = document.getElementById("meme-Display");
+const resetButton = document.getElementById("resetButton");
+const memeList = document.getElementById("memeList")
+
+
+function resetMemes(){
+    const memes = document.getElementsByClassName("memeLi");
+    console.log("Clicked Reset!");
+    while(memes.length > 0){
+        memes[0].parentNode.removeChild(memes[0]);
+    }
+    }
 
 submitButton.addEventListener("click", function(event){
     event.preventDefault();
     const topText = document.querySelector("#Top-Text");
     const bottomText = document.querySelector("#Bottom-Text");
     const imageURL = document.querySelector("#image-url");
-    const imageDisplay = document.querySelector("#Meme-Display");
-    
-    imageDisplay.innerHTML='';
-    // console.log(topText.value);
-    // console.log(bottomText.value);
-    // console.log(imageURL.value);
+    // const imageDisplay = document.querySelector("#meme-Display");
+    // imageDisplay.innerHTML='';
+
+    const imageLi = document.createElement("li");
     const imageDiv = document.createElement("div");
-    const memeDiv = document.createElement("div");
+
+    const deleteButton =  document.createElement("button")
+    const deleteButtonFront = document.createElement("Span");
+    deleteButtonFront.classList.add("front");
+    deleteButton.classList.add("pushable");
+    deleteButton.classList.add("deleteButton");
+    deleteButtonFront.innerHTML="Delete This!";
+    deleteButton.append(deleteButtonFront);
+
+    
+
+    deleteButton.addEventListener("click", function(e){
+         e.preventDefault();
+         console.log("Clicked Delete");
+         console.log(memeList.children[0]);
+         memeList.removeChild(this.parentElement);
+     });
 
     imageDiv.innerHTML = `
-            <div class="topMemeText">${topText.value}</div>
-            <img class="memeImage" src=${imageURL.value} />
-            <div class="bottomMemeText">${bottomText.value}</div>
+            <span class="topMemeText">${topText.value}</span>
+            <span class="bottomMemeText">${bottomText.value}</span>
+            <span> <img class="memeImage" src=${imageURL.value} /></span>
     `
-    imageDiv.classList.add("memeDiv");
-    console.log(imageDiv);
-    // topTextDiv.classList.add("topMemeText");
-    // bottomTextDiv.classList.add("bottomMemeText");
-
-    // memeDiv.append(topTextDiv);
-    // memeDiv.append(imageDiv);
-    // memeDiv.append(bottomTextDiv);
+  
  
-    imageDisplay.append(imageDiv);
-})
+
+    imageLi.classList.add("memeLi");
+    // imageDiv.append(deleteButton);
+    imageLi.appendChild(imageDiv);
+    imageLi.appendChild(deleteButton);
+
+    memeList.appendChild(imageLi);
+    formElement.reset();
+
+});
